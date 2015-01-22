178a09ee0b138533a44ad58f5ce14ae0dbe73f92
Richard Frank <richardfrank@me.com>
2015-01-22 13:28:57 -0700

    Removes the logger

--- lib/ghee/connection.rb
+++ lib/ghee/connection.rb
@@ -27,7 +27,6 @@ class Ghee
 
       super(hash[:api_url] || 'https://api.github.com') do |builder|
         yield builder if block_given?
-        builder.response :logger
         builder.use     Faraday::Response::RaiseGheeError
         builder.use     FaradayMiddleware::EncodeJson
         builder.use     FaradayMiddleware::ParseJson, :content_type => /\bjson$/
